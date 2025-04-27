24583b90d7961625fc8020ae14468d5497d3c9bd
Mateusz Dynur <mateusz.dynur@microsoft.wsei.edu.pl>
2025-04-27 10:46:27 +0200

    add guarding

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,5 +1,11 @@
-const DashboardPage = () => {
-  return <div>DashboardPage</div>;
-};
+import { useAuthGuard } from '../hooks/useAuthGuard';
 
-export default DashboardPage;
+export default function DashboardPage() {
+  useAuthGuard();
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-2xl font-bold">Welcome to your dashboard!</h1>
+    </div>
+  );
+}
